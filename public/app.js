97aa9dfe0db90889ae32190a551e7bda0ddc8eea
fadil <adian281r@gmail.com>
2021-12-29 14:26:20 +0700

    psuh dulu lah

---
+++ public/app.js
@@ -0,0 +1,7 @@
+const root = document.querySelector('#root');
+
+function App() {
+  return /*#__PURE__*/React.createElement("h1", null, "React COmponent");
+}
+
+ReactDOM.render( /*#__PURE__*/React.createElement(App, null), root);
---
+++ src/app.jsx
@@ -0,0 +1,8 @@
+const root = document.querySelector('#root');
+
+function App() {
+    return <h1>React COmponent</h1>
+}
+
+
+ReactDOM.render(<App />, root);
